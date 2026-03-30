20b4e0c1e71e98d3c0c654e0dab47e61abcc3b92
capital-poeeisan <captial.poeeieisan@gmail.com>
2026-03-30 17:13:53 +0630

    Add project files.

---
+++ EmployeeTrackingSystem/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeTrackingSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult StaffList()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult DetailList()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
