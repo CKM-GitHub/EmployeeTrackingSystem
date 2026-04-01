fb6d22a52b73634e0b1091a2948d3e232ec4d666
capital-poeeisan <captial.poeeieisan@gmail.com>
2026-04-01 14:40:49 +0630

    pes

---
+++ EmployeeTrackingSystem/Controllers/StaffController.cs
@@ -0,0 +1,50 @@
+﻿using EmployeeTrackingSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using static EmployeeTrackingSystem.Models.staffViewModel;
+
+namespace EmployeeTrackingSystem.Controllers
+{
+    public class StaffController : BaseController
+    {
+      
+        public ActionResult StaffList(string department, string staff)
+        {
+            ViewBag.ShowDropdown = true;
+            LoadDropdowns();
+
+            // JOIN Staff + Department
+            var staffList = (from s in db.T_StaffMaster
+                             join d in db.T_Department
+                             on s.DepartmentCD equals d.DepartmentCD
+                             select new StaffViewModel
+                             {
+                                 StaffCD = s.StaffCD,
+                                 StaffName = s.StaffName,
+                                 DepartmentCD = s.DepartmentCD,
+                                 DepartmentName = d.DepartmentName,
+                                 SeatNo = s.SeatNo
+                             }).ToList();
+
+            // 🔍 Filter Department
+            if (!string.IsNullOrEmpty(department) && department != "All")
+            {
+                staffList = staffList
+                    .Where(x => x.DepartmentCD == department)
+                    .ToList();
+            }
+
+            // 🔍 Filter Staff
+            if (!string.IsNullOrEmpty(staff) && staff != "All")
+            {
+                staffList = staffList
+                    .Where(x => x.StaffCD == staff)
+                    .ToList();
+            }
+           
+            return View(staffList);
+        }
+    }
+}
