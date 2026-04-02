bfb81f213bffaccb15f814c633dfc909bac331ca
ThiriThaungWin <ThiriThaungWin@ACT-JPH134C0S6>
2026-04-02 11:36:51 +0900

    20260402 ttw creae DetailContller, View, bootstrap5

---
+++ EmployeeTrackingSystem/Controllers/DetailController.cs
@@ -0,0 +1,75 @@
+﻿using EmployeeTrackingSystem.Controllers;
+using EmployeeTrackingSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeTrackingSystem.Controllers
+{
+    public class DetailController : BaseController
+    {
+        private Models.EmployeeTrackingDBEntities db = new EmployeeTrackingDBEntities();
+        // GET: Detail
+        public ActionResult DetailList()
+        {
+            ViewBag.ShowDropdown = true;
+            LoadDropdowns();
+            var deptParam = new SqlParameter("@Department", (object)DBNull.Value);
+            var staffParam = new SqlParameter("@StaffName", (object)DBNull.Value);
+            var frmDate = new SqlParameter("@FrmDate", (object)DBNull.Value);
+            var toDate = new SqlParameter("@ToDate", (object)DBNull.Value);
+
+            List<DetailViewModel> list = db.Database.SqlQuery<DetailViewModel>(
+             "EXEC DetailLoggingSelect @Department, @StaffName, @FrmDate, @ToDate",
+             deptParam, staffParam, frmDate, toDate
+         ).ToList();
+
+
+            return View(list);
+        }
+
+        [HttpPost]
+        public JsonResult GetFilteredData(string department, string staffname, string fromdate, string todate)
+        {
+            try
+            {
+                department = department == "All" ? null : department;
+                staffname = staffname == "All" ? null : staffname;
+                fromdate = fromdate == "" ? null : fromdate;
+                todate = todate == "" ? null : todate;
+                // Create SQL parameters to match your Stored Procedure
+                var deptParam = new SqlParameter("@Department", department ?? (object)DBNull.Value);
+                var staffParam = new SqlParameter("@StaffName", staffname ?? (object)DBNull.Value);
+                var frmDate = new SqlParameter("@FrmDate", fromdate ?? (object)DBNull.Value);
+                var toDate = new SqlParameter("@ToDate", todate ?? (object)DBNull.Value);
+
+                // Execute the SP and map results to your Model
+                List<DetailViewModel> list = db.Database.SqlQuery<DetailViewModel>(
+                "EXEC DetailLoggingSelect @Department, @StaffName, @FrmDate, @ToDate",
+                deptParam, staffParam, frmDate, toDate
+                ).ToList();
+
+                var result = list.Select(x => new {
+                    x.StaffName,
+                    x.DepartmentName,
+                    x.Status,
+                    StartDateTime = x.StartDateTime.HasValue
+                        ? x.StartDateTime.Value.ToString("yyyy-MM-dd HH:mm:ss")
+                        : "",
+                                    ReturnDateTime = x.ReturnDateTime.HasValue
+                        ? x.ReturnDateTime.Value.ToString("yyyy-MM-dd HH:mm:ss")
+                        : ""
+                });
+
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.Message, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ EmployeeTrackingSystem/Models/DetailViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeTrackingSystem.Models
+{
+    public class DetailViewModel
+    {
+        public int SEQ { get; set; }
+        public string DepartmentCD { get; set; }
+        public string DepartmentName { get; set; }
+        public string StaffCD { get; set; }
+        public string StaffName { get; set; }
+        public string Status { get; set; }
+        public Nullable<System.DateTime> StartDateTime { get; set; }
+        public Nullable<System.DateTime> ReturnDateTime { get; set; }
+        public string Note { get; set; }
+        public Nullable<System.DateTime> InsertDateTime { get; set; }
+        public Nullable<System.DateTime> DeleteDateTime { get; set; }
+    }
+}
