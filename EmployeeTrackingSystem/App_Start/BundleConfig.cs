7c99e4ca9448ada2353590290103d124747edc65
ThiriThaungWin <ThiriThaungWin@ACT-JPH134C0S6>
2026-04-15 18:01:24 +0900

    modify CSS using and installed required nuget. by ttw 20260415

--- EmployeeTrackingSystem/App_Start/BundleConfig.cs
+++ EmployeeTrackingSystem/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace EmployeeTrackingSystem
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui-1.13.2.js")); // Load UI right after jQuery));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,11 +21,18 @@ namespace EmployeeTrackingSystem
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                       "~/Scripts/bootstrap.js",                 // Load Bootstrap first
+            "~/Scripts/moment.min.js",                // Load Moment second
+            "~/Scripts/select2.min.js",               // Load Select2 third
+            "~/Scripts/bootstrap-datetimepicker.js"   // Load Datetimepicker last
+            ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            "~/Content/css/select2.min.css",
+            "~/Content/bootstrap-datetimepicker.min.css",
+            "~/Content/themes/base/jquery-ui.css",     
+            "~/Content/site.css"));
         }
     }
 }
