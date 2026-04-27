16a6f9ae369f210906c44b5fdb29b81f34c8b812
ThiriThaungWin <ThiriThaungWin@ACT-JPH134C0S6>
2026-04-27 17:07:13 +0900

    adjust block design in dashboard

--- EmployeeTrackingSystem/Controllers/HomeController.cs
+++ EmployeeTrackingSystem/Controllers/HomeController.cs
@@ -19,17 +19,17 @@ namespace EmployeeTrackingSystem.Controllers
         {            
             string json1 = Get_DashboardInformation(2, "D01");
             List<DashboardViewModel> table1 = JsonConvert.DeserializeObject<List<DashboardViewModel>>(json1);
-            string json2 = Get_DashboardInformation(8, "D02");
+            string json2 = Get_DashboardInformation(4, "D02");
             List<DashboardViewModel> table2 = JsonConvert.DeserializeObject<List<DashboardViewModel>>(json2);
             string json3 = Get_DashboardInformation(2, "D03");
             List<DashboardViewModel> table3 = JsonConvert.DeserializeObject<List<DashboardViewModel>>(json3);
-            string json4 = Get_DashboardInformation(4, "D04");
+            string json4 = Get_DashboardInformation(2, "D04");
             List<DashboardViewModel> table4 = JsonConvert.DeserializeObject<List<DashboardViewModel>>(json4);
-            string json5 = Get_DashboardInformation(4, "D05");
+            string json5 = Get_DashboardInformation(2, "D05");
             List<DashboardViewModel> table5 = JsonConvert.DeserializeObject<List<DashboardViewModel>>(json5);
             string json6 = Get_DashboardInformation(2, "D06");
             List<DashboardViewModel> table6 = JsonConvert.DeserializeObject<List<DashboardViewModel>>(json6);
-            string json7 = Get_DashboardInformation(8, "D07");
+            string json7 = Get_DashboardInformation(4, "D07");
             List<DashboardViewModel> table7 = JsonConvert.DeserializeObject<List<DashboardViewModel>>(json7);
            // string json8 = Get_DashboardInformation(2, "D08");
            // List<DashboardViewModel> table8 = JsonConvert.DeserializeObject<List<DashboardViewModel>>(json8);
